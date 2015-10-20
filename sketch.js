6faad46e1473c51779d21d475d413af7cad5618c
camerenisonfire <camerendolecheck@gmail.com>
2015-10-20 12:03:17 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var frequencyData;		//holds our Array of data about our audio frequency
+
+function setup() {
+  	createCanvas(720, 200);	//Make an HTML <canvas> 720x200px 
+}
+
+function draw() {
+	//Make a new Array to hold our frequency data	
+  	frequencyData = new Uint8Array(analyser.frequencyBinCount);	
+
+  	//Tell the analyser in radio.js to fill our Array with the frequency data	
+  	analyser.getByteFrequencyData(frequencyData);
+	
+	//Sets the fill color of our shape red with low opacity
+  	fill(255, 0, 0, 10);	
+  	//Draw a rectangle, which will be red with low opacity	
+  	rect(0, 0, width, height);
+
+  	fill(255);		//Set the fill color to white
+  	noStroke();		//Turn off the outline of our shape
+
+  	//Draw an ellipse in the center of our canvas that scales with the frequency data 
+  	ellipse(width/2, height/2, 1+frequencyData[10], 1+frequencyData[10] );
+
+
+
+  	/* More explanation of the ellipse function above:
+  	The parameters to are ellipse(xPosition, yPosition, width, height)
+  	width/2 and height/2 sets the coordinates to the center of the canvas
+  	The size of the ellipse is 1+frequencyData[10]
+  	meaning the minimum size of the ellipse is 1 and we then add 
+  	the number analyyser put into in the 10th bin of frequencyData
+  	The higher the bin number, the higher the frequency the circle will react to.
+  	*/
+}
